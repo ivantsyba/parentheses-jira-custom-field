1a49dac0ce5449d82bb342b39ec67157d1ddcc81
Ivan Tsyba <ivantsyba@gmail.com>
2017-08-17 22:39:13 +0300

    Generated skeleton for Custom Field plugin module

---
+++ src/test/java/ut/com/idalko/plugins/jira/customfields/ParenthesesCustomFieldTest.java
@@ -0,0 +1,34 @@
+package ut.com.idalko.plugins.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.idalko.plugins.jira.customfields.ParenthesesCustomField;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class ParenthesesCustomFieldTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //ParenthesesCustomField testClass = new ParenthesesCustomField();
+
+        throw new Exception("ParenthesesCustomField has no tests!");
+
+    }
+
+}
